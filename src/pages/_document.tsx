5dc697864ed007ebc103e5a6a0ad24026390bc89
Yuri-Tiofilo <yusilva12345@gmail.com>
2021-03-26 01:38:47 -0300

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, {
+  Html,
+  Main,
+  NextScript,
+  Head
+} from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
