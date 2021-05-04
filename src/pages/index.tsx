1d93287c3418b2a276eea9171d26fa5368ee29f2
Yuri SIlva <yusilva12345@gmail.com>
2021-05-04 09:39:07 -0300

    04/05

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
-export default function Home(): JSX.Element {
+export default function Home(props): JSX.Element {
+  console.log(props)
   return (
     <>
       <Head>
@@ -13,7 +15,9 @@ export default function Home(): JSX.Element {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> word.</h1>
+          <h1>
+            News about the <span>React</span> word.
+          </h1>
           <p>
             Get access to all the publications. <br />
             <span>for $9.90 month</span>
@@ -22,8 +26,16 @@ export default function Home(): JSX.Element {
           <SubscribeButton />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding"/>
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      name: 'Diego'
+    }
+  }
 }
