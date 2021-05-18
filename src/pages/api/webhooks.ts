6886512285a618105319f70700befdbf9d172e0f
Yuri SIlva <yusilva12345@gmail.com>
2021-05-17 22:51:50 -0300

    final config stripe and faunaDB

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { Readable } from 'stream'
 import Stripe from 'stripe'
 import { stripe } from '../../services/stripe'
+import { saveSubscription } from './_lib/manangeSubscription'
 
 async function buffer(readable: Readable) {
   const chunks = []
@@ -19,7 +20,11 @@ export const config = {
   }
 }
 
-const relevantEvents = new Set(['checkout.session.completed'])
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'costumer.subscription.updated',
+  'costumer.subscription.deleted'
+])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -42,8 +47,34 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event
 
     if (relevantEvents.has(type)) {
-      // faz algo
-      console.log('Evento recebido', event)
+      try {
+        switch (type) {
+          case 'costumer.subscription.created':
+          case 'costumer.subscription.updated':
+          case 'costumer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              type === 'costumer.subscription.created'
+            )
+            break
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            )
+            break
+          default:
+            throw new Error('Unhandled event.')
+        }
+      } catch (err) {
+        return res.json({ error: 'Webhook handler failed.' })
+      }
     }
 
     res.json({ received: true })
